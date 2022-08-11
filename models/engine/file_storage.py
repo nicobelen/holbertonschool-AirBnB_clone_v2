de24312c7903ea330488e9ed9a4910bfa2f89ed3
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-08-11 12:36:16 -0300

    Test on all file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -29,7 +29,7 @@ class FileStorage:
             temp = dict()
             for key in self.__objects:
                 _class = key.split(".")
-                if _class[0] == classes:
+                if _class[0] in classes:
                     temp[key] = self.__objects[key]
             return temp
         else:
