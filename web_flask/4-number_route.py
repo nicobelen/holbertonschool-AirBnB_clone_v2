9ad5e46183491439f8726a9e4fce3e0d828095ad
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-09-05 15:01:32 -0300

    Pushing 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+app.strict_slashes = False
+
+
+@app.route("/")
+def hello_world():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def text(text):
+    string = text.replace("_", " ")
+    return "C {}".format(string)
+
+
+@app.route("/python/")
+@app.route("/python/<text>")
+def python(text="is cool"):
+    string = text.replace("_", " ")
+    return "Python {}".format(string)
+
+
+@app.route("/number/n:int")
+def isnumber(n):
+    return "{} is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
