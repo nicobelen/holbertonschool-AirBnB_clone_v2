4f2b912defda5259c339190684a51bad5b849490
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-09-05 13:00:05 -0300

    Pushing 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+app.strict_slashes = False
+
+
+@app.route("/")
+def hello_world():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def text(text):
+    string = text.replace("_", " ")
+    return "C {}".format(string)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
